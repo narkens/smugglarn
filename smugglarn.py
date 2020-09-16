89242f7f40af9d18b2b336767e6cd11f70bc4f67
narken <narken.spam@gmail.com>
2020-09-16 17:19:44 +0200

    Minor changes

--- smugglarn.py
+++ smugglarn.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
 import requests
 
 r = requests.get('http://127.0.0.1:8000/test/../test')
 print(r.text)
+
+# Encodings - add orange stuff
+dot = ['.','%2e','%u002e']
+slash = ['/','%2f','%u002f']
